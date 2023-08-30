62eb48f49e87a3d6adac9ed360fdd4a0450996b3
unknown <martinHabal>
2023-08-30 11:47:41 +0200

    sablona rozvrhu update

--- modules/routes.js
+++ modules/routes.js
@@ -8,14 +8,14 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 const connection = mysql.createConnection({
-    host: 'localhost', // Název nebo IP adresa serveru databáze
-    user: 'monty', // Uživatelské jméno
-    password: 'monty88', // Heslo
-    database: 'timetable', // Název databáze
-    port: 3001
-  });
+  host: 'localhost', // Název nebo IP adresa serveru databáze
+  user: 'monty', // Uživatelské jméno
+  password: 'monty88', // Heslo
+  database: 'timetable', // Název databáze
+  port: 3001
+});
 
-  
+
 // Definice rout a obslužných funkcí
 router.get('/timetable', (req, res) => {
 
@@ -25,68 +25,94 @@ router.get('/timetable', (req, res) => {
       return;
     }
     console.log(results)
-    
+
     res.render('timetable', { results });
   })
-// const scheduleData = {
-//     days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-//     timetable: [
-//       ['Math', 'Physics', 'Chemistry', ''],
-//       ['English', 'History', 'Geography', ''],
-//       ['Biology', 'Physics', '', ''],
-//       ['', '', 'Chemistry', ''],
-//       ['Math', 'English', 'History', '']
-//     ]
-//   };
-//     res.render('timetable', { scheduleData });
+  // const scheduleData = {
+  //     days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  //     timetable: [
+  //       ['Math', 'Physics', 'Chemistry', ''],
+  //       ['English', 'History', 'Geography', ''],
+  //       ['Biology', 'Physics', '', ''],
+  //       ['', '', 'Chemistry', ''],
+  //       ['Math', 'English', 'History', '']
+  //     ]
+  //   };
+  //     res.render('timetable', { scheduleData });
 });
 // new timetable
 router.get('/newtimetable', (req, res) => {
-    let data = {
-        days: ["Po", "Út", "S", "Čt", "Pá"],
-        hours: [0,1,2,3,4,5,6,7,8,9,10,11,12,13],
-        period: ["odd", "even"],  
-    }
-    res.render('newtimetable', { data });
+  let data = {
+    days: ["Po", "Út", "S", "Čt", "Pá"],
+    hours: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    period: ["odd", "even"],
+  }
+  res.render('newtimetable', { data });
 });
 
 // new timetable
 router.get('/admin', (req, res) => {
-    connection.query('SELECT * FROM subjects', (error, results, fields) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        console.log(results)
-        res.render('admin', { results });
-    
-      })
+  connection.query('SELECT * FROM subjects', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(results)
+    res.render('admin', { results });
+
+  })
 });
 router.post('/newtimetable', function (request, response, next) {
 
 
+  console.log(request.body)
 
-    console.log(request.body)
-   
-        // SQL dotaz pro vložení dat do databáze
-        var sql = `INSERT INTO timetable (day, hour, class, period) VALUES ('${request.body.day}', '${request.body.hour}', '${request.body.class}', '${request.body.period}')`;
-      
-        connection.query(sql, (error, results, fields) => {
-          if (error) {
-            console.error(error);
-            return;
-          }
-          console.log(results);
-        })
-        let data = {
-            days: ["Po", "Út", "S", "Čt", "Pá"],
-            hours: [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
-            
-           
-        }
-        response.render('newtimetable', { data });
-        // response.send("hotovo")
-      
-      })
+  // SQL dotaz pro vložení dat do databáze
+  var sql = `INSERT INTO timetable (day, hour, class, period) VALUES ('${request.body.day}', '${request.body.hour}', '${request.body.class}', '${request.body.period}')`;
+
+  connection.query(sql, (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(results);
+  })
+  let data = {
+    days: ["Po", "Út", "S", "Čt", "Pá"],
+    hours: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+
+  }
+  response.render('newtimetable', { data });
+  // response.send("hotovo")
+
+})
+
+//update rozvrhu
+router.post('/timetableupdate', function (request, response, next) {
+
+
+  console.log(request.body.day)
+  console.log(request.body.hour)
+
+  // Aktualizace záznamu v tabulce
+const idToUpdate = 6; // ID záznamu, který chcete aktualizovat
+const newValues = {
+  day: 'po',
+  hour: 'po'
+};
+  // SQL dotaz pro vložení dat do databáze
+  const sqlQuery = 'UPDATE timetable_odd SET ? WHERE id = ?';
+  connection.query(sqlQuery, [newValues, idToUpdate], (err, result) => {
+    if (err) {
+      console.error('Chyba při aktualizaci záznamu: ' + err.stack);
+      return;
+    }
+    console.log('Záznam byl úspěšně aktualizován.');
+  });
+  response.render('newtimetable', { data });
+  // response.send("hotovo")
+
+})
 // Exportování routeru
 module.exports = router;
